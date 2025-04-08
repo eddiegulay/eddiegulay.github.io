fa10987ca28c297bd61ce9415a749fa603cfb944
eddiegulay <edgargulay@outlook.com>
2025-04-08 13:39:06 +0200

    feat: loaded alan watts

---
+++ assets/js/alan.js
@@ -0,0 +1,42 @@
+const originalText = `
+If you’re ready to wake up, you’re going to wake up and if you’re not ready, you’re going to stay pretending that you're just “poor little me”.
+
+And since you're all here and engaged in this sort of inquiry and listening to this sort of lecture, I
+assume that you’re all on the process of waking up or else you’re teasing yourself with some kind of flirtation with waking up which you’re not serious about.
+
+But I assume maybe you are not serious, but sincere that you are ready to wake up. So then, when you’re in the way of waking up and finding out who you really are, what you do is what the whole universe is doing at the place you call here and now.
+
+You are something the whole universe is doing in the same way that a wave is something that the whole ocean is doing.
+
+The real you is not a puppet which life pushes around.
+
+The real, deep down you is the whole universe.
+
+So then, when you die, you’re not going to have to put up with everlasting non-existing. Because that's not an experience. A lot of people are afraid that when they die they’re going to be locked up in a dark room forever and sort of undergo that. But one of the most interesting things in the world – this is yoga, this is a way of realization.
+
+Try and imagine what it will be like to go to sleep and never wake up.
+
+Think about that.
+
+Children think about that.
+
+It’s one of the great wonders of life.
+
+What will it be like to go to sleep and never wake up?
+
+And if you think long enough about that, something will happen. You will find out, among other things. That it will pose the next question to you. What was it like to wake up after having never gone to sleep?
+
+That was when you were born.
+
+You see? You cant have an experience of nothing; Nature abhors a vacuum. So after you're dead, the only thing that can happen is the same experience, or the same sort of experience of before you were born. In other words, we all know very well that after people die. Other people are born.
+And they're all you, only you can only experience one at a time.
+
+Everybody is I, you all you’re you.
+
+And wheresoever’s beings exist throughout all galaxies it doesn't make a difference.
+
+You are all of them and when they come into being as you come into being.
+
+You know that very well.
+
+Only you don’t have to remember the past in the same way you don’t have to think about you work thyroid gland or whatever else it is in your organism. You don’t have to know how to shine the sun, "You just do it". Like you breathe. Doesn't it really astonish you that you are the fantastically complex thing and that you’re doing all of this and you never had any education on how to do it.`;
